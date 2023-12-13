fb2beb470b75bc04da007e40832da83fa4b83659
Mariam Albarghouti <mariamalbarghouti.com>
2023-12-13 13:25:46 +0200

    rename los_ui

--- los_prediction/wsgi.py
+++ los_prediction/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for los_ui project.
+WSGI config for los_prediction project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "los_ui.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "los_prediction.settings")
 
 application = get_wsgi_application()
